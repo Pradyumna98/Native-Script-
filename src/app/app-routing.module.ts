fdf6f37ea7e1ad767177a2b5e569ee522eadaec1
Pradyumna98 <vknarayana98@gmail.com>
2020-08-29 23:48:50 +0530

    Navigation Part 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { NativeScriptRouterModule } from "@nativescript/angular";
 
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HomeComponent } from './home/home.component';
+
 
 
 const routes: Routes = [
     { path: "", redirectTo: "/menu", pathMatch: "full" },
     { path: "menu", component: MenuComponent },
     { path: 'dishdetail/:id',     component: DishdetailComponent },
-    
+    { path: 'dishdetail/:id',     component: DishdetailComponent }
 ];
 
 @NgModule({
